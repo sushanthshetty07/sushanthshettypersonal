ccff46d322d0b76a1d9f6cf4996c027e70f50c7c
shettysushanth7 <sushanth@insightiq.ai>
2025-07-15 15:15:51 +0530

    code refactored by structurring and cleaned the redundancy

--- HelpDeskProductPageObject/src/main/java/CustomerPortal/SupportPortalPage.java
+++ HelpDeskProductPageObject/src/main/java/CustomerPortal/SupportPortalPage.java
@@ -1,131 +1,122 @@
 package CustomerPortal;
-import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import Admin_Portal.AdminPortalTest2ndPage;
-import Admin_Portal.Login_Page;
 import webdriverbase.AppPage;
 
+/**
+ * Page object for the Support Portal ticket creation page.
+ */
+public class SupportPortalPage extends AppPage {
 
-public class SupportPortalPage extends AppPage{
-	
-	public SupportPortalPage(WebDriver driver)
-	{
-		super(driver);
-				
-	}
-	
-	
-	public void navigateToHappyFoxSupportPortalURL(String url) {
-		this.driver.get(url);
-	}
-		
-	@FindBy(id = "id_subject")  
-	private WebElement Subject;
-	
-	public void enterSubject(String text)
- 	{
-		Subject.sendKeys(text);
- 	}
-	
-	@FindBy(xpath = "//div[@class='cke_wysiwyg_div cke_reset cke_enable_context_menu cke_editable cke_editable_themed cke_contents_ltr cke_show_borders']")  
-	private WebElement Message;
-	
-	public void enterMessage(String text)
- 	{
-	    Message.sendKeys(text);
- 	}
-	
-	@FindBy(id = "add-cc")  
-	private WebElement AddCC;
-	
-	public void clickAddCC()
- 	{
-	    AddCC.click();
- 	}
-	
-	@FindBy(id = "add-bcc")  
-	private WebElement AddBCC;
-	
-	public void clickAddBCC()
- 	{
-	    AddBCC.click();
- 	}
-	
-	@FindBy(xpath = "//input[@id='id_cc']")  
-	private WebElement CC;
-	
-	public void enterCC(String text)
- 	{
-	    CC.sendKeys(text);
- 	}
-	
-	@FindBy(xpath = "//input[@id='id_bcc']")  
-	private WebElement BCC;
-	
-	public void enterBCC(String text)
- 	{
-	    BCC.sendKeys(text);
- 	}
-	
-	
-	
-	@FindBy(xpath = "//a[@class='hf-attach-file_link']")  
-	private WebElement BrowseFile;
-	
-	public void addingScreenshot(String abc)
- 	{
-	    BrowseFile.sendKeys(getTestDataFullDirPath(abc));
-	    
- 	}
-	
-	
-	@FindBy(id = "id_name")  
-	private WebElement FullName;
-	
-	public void enterFullName(String text) throws InterruptedException
- 	{
-		sleep();
-	    FullName.sendKeys(text);
- 	}
-	
-	@FindBy(id = "id_email")  
-	private WebElement Email;
-	
-	public void enterEmail(String text) throws InterruptedException
- 	{
-		sleep();
-	    Email.sendKeys(text);
- 	}
-	
-	@FindBy(id = "id_phone")  
-	private WebElement Phone;
-	
-	public void enterPhone(String text)
- 	{
-	    Phone.sendKeys(text);
- 	}
-	
-	
-	@FindBy(xpath = "//button[@id='submit']")  
-	private WebElement CreateTicket;
-		
-	public AdminPortalTest2ndPage clickCreateTicket()
-	 {
-		   CreateTicket.click();
-		   return new AdminPortalTest2ndPage(driver);
-	 }
-	
-     public void sleep() throws InterruptedException {
-    	 Thread.sleep(1000);
-     }
-     
-     
-     
-	
-	
+    private static final int TIMEOUT = 10;
+    private WebDriverWait wait;
+
+    public SupportPortalPage(WebDriver driver) {
+        super(driver);
+        this.wait = new WebDriverWait(driver, TIMEOUT);
+    }
+
+    /**
+     * Navigates to the HappyFox Support Portal ticket creation page.
+     */
+    public void navigateToHappyFoxSupportPortalURL(String url) {
+        driver.get(url);
+    }
+
+    @FindBy(id = "id_subject")
+    private WebElement subject;
+
+    public void enterSubject(String text) {
+        wait.until(ExpectedConditions.visibilityOf(subject));
+        subject.sendKeys(text);
+    }
+
+    @FindBy(xpath = "//div[@class='cke_wysiwyg_div cke_reset cke_enable_context_menu cke_editable cke_editable_themed cke_contents_ltr cke_show_borders']")
+    private WebElement message;
+
+    public void enterMessage(String text) {
+        wait.until(ExpectedConditions.visibilityOf(message));
+        message.sendKeys(text);
+    }
+
+    @FindBy(id = "add-cc")
+    private WebElement addCC;
+
+    public void clickAddCC() {
+        wait.until(ExpectedConditions.elementToBeClickable(addCC));
+        addCC.click();
+    }
+
+    @FindBy(id = "add-bcc")
+    private WebElement addBCC;
+
+    public void clickAddBCC() {
+        wait.until(ExpectedConditions.elementToBeClickable(addBCC));
+        addBCC.click();
+    }
+
+    @FindBy(xpath = "//input[@id='id_cc']")
+    private WebElement cc;
+
+    public void enterCC(String text) {
+        wait.until(ExpectedConditions.visibilityOf(cc));
+        cc.sendKeys(text);
+    }
+
+    @FindBy(xpath = "//input[@id='id_bcc']")
+    private WebElement bcc;
+
+    public void enterBCC(String text) {
+        wait.until(ExpectedConditions.visibilityOf(bcc));
+        bcc.sendKeys(text);
+    }
+
+    @FindBy(xpath = "//a[@class='hf-attach-file_link']")
+    private WebElement browseFile;
+
+    public void addingScreenshot(String filePath) {
+        wait.until(ExpectedConditions.elementToBeClickable(browseFile));
+        browseFile.sendKeys(getTestDataFullDirPath(filePath));
+    }
+
+    @FindBy(id = "id_name")
+    private WebElement fullName;
+
+    public void enterFullName(String text) {
+        wait.until(ExpectedConditions.visibilityOf(fullName));
+        fullName.sendKeys(text);
+    }
+
+    @FindBy(id = "id_email")
+    private WebElement email;
+
+    public void enterEmail(String text) {
+        wait.until(ExpectedConditions.visibilityOf(email));
+        email.sendKeys(text);
+    }
+
+    @FindBy(id = "id_phone")
+    private WebElement phone;
+
+    public void enterPhone(String text) {
+        wait.until(ExpectedConditions.visibilityOf(phone));
+        phone.sendKeys(text);
+    }
+
+    @FindBy(xpath = "//button[@id='submit']")
+    private WebElement createTicket;
+
+    /**
+     * Clicks the create ticket button and returns the next page object.
+     */
+    public AdminPortalTest2ndPage clickCreateTicket() {
+        wait.until(ExpectedConditions.elementToBeClickable(createTicket));
+        createTicket.click();
+        return new AdminPortalTest2ndPage(driver);
+    }
 }
--- HelpDeskProductTests/src/main/java/Admin_Portal/AdminPortalTest1.java
+++ HelpDeskProductTests/src/main/java/Admin_Portal/AdminPortalTest1.java
@@ -1,35 +1,18 @@
 package Admin_Portal;
 
-import org.seleniumhq.jetty9.util.log.LoggerLog;
-import org.testng.Assert;
 import org.testng.annotations.Test;
+import CommonTests.CommonTests1;
 
-import CommonTests.CommonTests1; 
+/**
+ * Test for Scenario 1: Login, create status/priority, delete them, and logout.
+ */
+public class AdminPortalTest1 extends CommonTests1 {
 
- public class AdminPortalTest1 extends CommonTests1  {
-
-  @Test
-  public void Scenario1() {
-	  
-	   String Methodname = new Object(){}.getClass().getEnclosingMethod().getName(); 
-	
-		try {
-			
-			String Classname = getClass().getSimpleName();
-            System.out.println("start of method : " + Classname);
-            loginasAdmin();
-            TestCase1();
-            TestCase3();
-				
-		} 
-		
-		catch (Throwable e) {
-			e.printStackTrace();
-			Assert.fail("Error in "+ Methodname +" : "+ e.getMessage());
-		}
-		
-	
-  } 
-  
- }
+    @Test
+    public void scenario1() {
+        loginAsAdmin();
+        createAndSetDefaultStatusAndPriority();
+        deleteStatusPriorityAndLogout();
+    }
+}
  
